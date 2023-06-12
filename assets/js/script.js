3f1b2a3ea761d5357c8232ae009f4c18c5cfc75f
Sherwin Abraham <sherwinabraham@gmail.com>
2023-06-12 11:56:38 -0500

    Debugging score.

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,63 @@ var wrongAns = 10; //Penalty for a wrong answer
 var endGame = false; //Ends everything
 var timeEle = document.getElementById("timer-el");
 var timerObject;
+
+const questAnnoying = [
+    //https://quizlet.com/779286477/javascript-fundamentals-flash-cards/
+    {
+      question: "Which is not a primitive data type in JavaScript?",
+      answers: [
+        { text: "Strings", accurate: false },
+        { text: "Numbers", accurate: false },
+        { text: "Booleans", accurate: false },
+        { text: "Functions", accurate: true },
+        { text: "Symbols", accurate: false },
+      ],
+    },
+    {
+      question:
+        "Suppose 'object' is a variable array. How would that be displayed on the console?",
+      answers: [
+        { text: "console.log(object.length);", accurate: true },
+        { text: "object.length;", accurate: false },
+        { text: "array.length;", accurate: false },
+        { text: "function{variable.object.length};", accurate: false },
+        { text: "Undefined.", accurate: false },
+      ],
+    },
+    {
+      question: "Which characteristics are in a 'for' loop?",
+      answers: [
+        { text: "Starting point", accurate: false },
+        { text: "Limit", accurate: false },
+        { text: "Increment", accurate: false },
+        { text: "None of the above.", accurate: false },
+        { text: "All of the above", accurate: true },
+  ],
+    },
+    {
+      question: "The operator '===' means that comparative objects are ___.",
+      answers: [
+        { text: "Equal in type", accurate: false },
+        { text: "Equal in value", accurate: false },
+        { text: "Equal in type and value", accurate: true },
+        { text: "Cheeseburgers", accurate: false },
+        { text: "This is twisted math type stuff.", accurate: false },
+      ],
+    },
+    {
+      question: "Who invented JavaScript?",
+      answers: [
+        { text: "Spencer Sokol", accurate: false },
+        { text: "Brendan Eich", accurate: true },
+        { text: "William H. Gates III", accurate: false },
+        { text: "Mick Foley", accurate: false },
+        { text: "Donald Trump", accurate: false },
+      ],
+    },
+  ];
+  
+
 function starter() {
   document.getElementById("Test-Title").classList.add("hide");
   document.getElementById("questions").classList.remove("hide");
@@ -41,6 +98,7 @@ function nextQuest() {
     endQuiz();
     startItOut.innerText = "Restart";
     startItOut.classList.remove("hide");
+    countScore();
   }
 }
 
@@ -104,60 +162,6 @@ function resetState() {
     answerEl.removeChild(answerEl.firstChild);
   }
 }
-const questAnnoying = [
-  //https://quizlet.com/779286477/javascript-fundamentals-flash-cards/
-  {
-    question: "Which is not a primitive data type in JavaScript?",
-    answers: [
-      { text: "Strings", accurate: false },
-      { text: "Numbers", accurate: false },
-      { text: "Booleans", accurate: false },
-      { text: "Functions", accurate: true },
-      { text: "Symbols", accurate: false },
-    ],
-  },
-  {
-    question:
-      "Suppose 'object' is a variable array. How would that be displayed on the console?",
-    answers: [
-      { text: "console.log(object.length);", accurate: true },
-      { text: "object.length;", accurate: false },
-      { text: "array.length;", accurate: false },
-      { text: "function{variable.object.length};", accurate: false },
-      { text: "Undefined.", accurate: false },
-    ],
-  },
-  {
-    question: "Which characteristics are in a 'for' loop?",
-    answers: [
-      { text: "Starting point", accurate: false },
-      { text: "Limit", accurate: false },
-      { text: "Increment", accurate: false },
-      { text: "None of the above.", accurate: false },
-      { text: "All of the above", accurate: true },
-],
-  },
-  {
-    question: "The operator '===' means that comparative objects are ___.",
-    answers: [
-      { text: "Equal in type", accurate: false },
-      { text: "Equal in value", accurate: false },
-      { text: "Equal in type and value", accurate: true },
-      { text: "Cheeseburgers", accurate: false },
-      { text: "This is twisted math type stuff.", accurate: false },
-    ],
-  },
-  {
-    question: "Who invented JavaScript?",
-    answers: [
-      { text: "Spencer Sokol", accurate: false },
-      { text: "Brendan Eich", accurate: true },
-      { text: "William H. Gates III", accurate: false },
-      { text: "Mick Foley", accurate: false },
-      { text: "Donald Trump", accurate: false },
-    ],
-  },
-];
 
 function startTimer() {
   timerObject = setInterval(function () {
@@ -177,15 +181,16 @@ function endQuiz() {
 var score = 0;
 //const valid = chooseAnswer.dataset.correct;
 
-//function countScore(){
-//    for (var i=0; i < questAnnoying.length; i++){
-//       if(valid == true){
-//        score++;
-//        prompt("Your score is "+ score)
-//        console.log("Your score is "+ score);
-//        }
-//    }
-//}
+function countScore(event){
+    const chooseAnswer = event.target;
+    for (var i=0; i < questAnnoying.length; i++){
+        if(valid == true){
+        score++;
+        prompt("Your score is "+ score*20)
+        console.log("Your score is "+ score*20);
+        }
+    }
+}
 /*function startOff(){
     .forEach(element => {
         if (answer.checked){
